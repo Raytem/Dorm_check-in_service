da7f375e01b39334df8edf29c4a0855198bd60e0
Daniil Rummo <daniil.r@gta.live>
2025-05-31 19:58:17 +0300

    changed role value

--- src/domain/enums/role.enum.ts
+++ src/domain/enums/role.enum.ts
@@ -1,6 +1,6 @@
 export enum Role {
   CIT = 'ROLE_CIT',
-  DEPUTY_DEAN = 'ROLE_TEST', // TODO: заменить на 'DEPUTY_DEAN'
+  DEPUTY_DEAN = 'DEPUTY_DEAN',
   HOSTEL = 'ROLE_HOSTEL',
   STUDENT = 'ROLE_STUDENT',
 }
